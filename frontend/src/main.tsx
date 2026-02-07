e385a497a555f6b9f5772d0369d88b4067932b5f
dev-SachinPrajapati <websiedeveloper0101@gmail.com>
2026-02-08 00:26:16 +0530

    removed strict mode from main entry

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+// import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
@@ -6,9 +6,9 @@ import "@maptiler/sdk/dist/maptiler-sdk.css";
 import { QueryProvider } from './providers/QueryProvider';
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+  // <StrictMode>
     <QueryProvider>
     <App />
     </QueryProvider>
-  </StrictMode>
+  // </StrictMode>
 );
